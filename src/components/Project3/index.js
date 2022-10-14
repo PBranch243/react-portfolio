846d65159d9ba2076d7862dcf980a8ea34018efd
unknown <philbranch243@gmail.com>
2022-10-13 19:15:40 -0500

    deploy with new project

--- src/components/Project3/index.js
+++ src/components/Project3/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import screenshot from "../../assets/<imgfile>"
+// import screenshot from "../../assets/<imgfile>"
 
 const styles = {
     p: {
@@ -19,9 +19,9 @@ function Project3() {
             <h1 style={styles.p}>{project.name}</h1>
             <p style={styles.p}>{project.description}</p>
             <a href={project.link}>
-            <img
+            {/* <img
             src={screenshot} alt="screenshot"
-            width="604" height="324"/>
+            width="604" height="324"/> */}
             </a>
         </section>
     );
